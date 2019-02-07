6abac762674828993a5912b6c64651e1b76803a7
idk <problemsolver@openmailbox.org>
2019-02-07 10:15:08 -0500

    move to upstream IPFS libraries

--- i2p/i2p.go
+++ i2p/i2p.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rtradeltd/go-ipfs-plugin-i2p-gateway/config"
 	//TODO: Get a better understanding of gx.
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
-	config "github.com/ipfs/go-ipfs/gxlibs/github.com/ipfs/go-ipfs-config"
+	config "github.com/ipfs/go-ipfs/go-ipfs-config"
 	plugin "github.com/ipfs/go-ipfs/plugin"
 	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
 )
